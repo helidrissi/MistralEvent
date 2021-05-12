80cd83df30369b30e1986e115ce741ec32da8748
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2021-05-12 19:56:12 +0200

    vérif mail mistral connexion à l'inscription

--- MistralEventFront/src/app/components/authentication/views/registration/registration.component.ts
+++ MistralEventFront/src/app/components/authentication/views/registration/registration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-registration',
@@ -27,12 +28,24 @@ export class RegistrationComponent implements OnInit {
     if(this.checkEmailMistral()) {
       if (this.checkPassword()) {
        console.log(this.convertFormToNewUser());
-        this.authService.registration(this.convertFormToNewUser()).subscribe((res) => {
-          this.router.navigate(['/login']);
-        });
+      //  TODO ajouter une vérif si le compte existe
+        this.authService.registration(this.convertFormToNewUser()).pipe(          
+          switchMap(() => {
+            return this.authService.login(this.formRegistration.value.email, this.formRegistration.value.password);
+          })
+        )
+        .subscribe(() => {
+            this.router.navigateByUrl("/home/agenda");
+        },
+        error => {
+          console.log('error', error);
+          this.errorMessage = 'Une erreur est survenu pendant votre inscription'
+        }
+        );
       }
     }
   }
+
   checkPassword() {
     if (this.formRegistration.value.password !== this.formRegistration.value.passwordConfirmation) {
       this.errorMessage = "Les mots de passes doivent être identiques";
@@ -42,12 +55,17 @@ export class RegistrationComponent implements OnInit {
   }
 
   checkEmailMistral() {
-    if (!this.formRegistration.value.email.endsWith('@mistral.fr')) {
+    const nameForEmail = this.formRegistration.value.lastName.toLowerCase();
+    const firstNameForEmail = this.formRegistration.value.firstName.toLowerCase();
+    const endsEmail = "@mistral.fr";
+    const emailExpected = firstNameForEmail + '.' + nameForEmail + endsEmail;
+    if (this.formRegistration.value.email !== emailExpected ) {
       this.errorMessage = "Vous devez avoir une adresse email Mistral ! "
       return false;
     }
     return true
   }
+
   convertFormToNewUser() {
     const newUser = {
       firstName: this.formRegistration.value.firstName,
@@ -55,7 +73,14 @@ export class RegistrationComponent implements OnInit {
       email: this.formRegistration.value.email,
       password: this.formRegistration.value.password,
     }
-
     return newUser;
+  }
+
+  checkLengthMdp(event) {
+    if (event.target.value.length < 6) {
+      this.errorMessage = "Le mot de passe saisie n'est pas assez long"
+    } else {
+      this.errorMessage = null
+    }
   }
 }
